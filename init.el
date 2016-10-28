b2a04147feab41664fc5009e338c76fe1ce2e148
Cassino <renato.santos@corp.globo.com>
2016-10-28 15:00:11 -0200

    Emacs configuration

---
+++ init.el
@@ -0,0 +1,36 @@
+;;;(setq make-backup-files nil)
+
+(add-to-list 'load-path "~/.emacs.d/themes/")
+(add-to-list 'load-path "~/.emacs.d/elpa/neotree")
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(global-linum-mode t)
+(load-theme 'wombat)
+
+;; Find file
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
+
+;; Space between line
+(setq linum-format "%4d \u2502 ")
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
